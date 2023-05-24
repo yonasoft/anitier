7ff0693fe422377b22e8c88cfb6a5b5eaa465ccc
KevC000 <chenkevin1119@gmail.com>
2023-05-24 14:25:42 -0400

    hello  world

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,13 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
