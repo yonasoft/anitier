6f5c1809006c8b8063744afbc71752928ee97a94
KevC000 <chenkevin1119@gmail.com>
2023-06-10 10:34:31 -0400

    pulled changes and minor adjustments

--- app/controllers/api/tiers_controller.rb
+++ app/controllers/api/tiers_controller.rb
@@ -12,9 +12,10 @@ module Api
     end
 
     def create
-      @tier = Tier.new(tier_params)
+      @tier = Tier.new(tier_params.except(:content_ids))
 
       if @tier.save
+        @tier.contents = Content.where(id: tier_params[:content_ids])
         render json: @tier, status: :created
       else
         render json: @tier.errors, status: :unprocessable_entity
@@ -42,8 +43,8 @@ module Api
       @tier = Tier.find(params[:id])
     end
 
-    def tier_params
-      params.require(:tier).permit(:rank, :tier_list_id)
+    def tier_params   
+      params.require(:tier).permit(:rank, :tier_list_id, content_ids: [])
     end
   end
 end
